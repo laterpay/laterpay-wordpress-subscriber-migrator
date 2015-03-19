f2b2b89b7f80ae3439ebbfeff53ce0297eb02e6f
drodler <drodler@laterpay.net>
2015-03-19 19:48:33 +0100

    Added separate template for sitenotice

---
+++ views/frontend/partials/sitenotice.php
@@ -0,0 +1,12 @@
+<?php
+    if ( ! defined( 'ABSPATH' ) ) {
+        // prevent direct access to this file
+        exit;
+    }
+?>
+
+<div class="lp_sitenotice">
+    SITENOTICE TEXT HERE
+    <a href="#" id="lp_buySubscription button button-primary">BUTTON TEXT HERE</a>
+    <a href="#" id="lp_fakeButton" class="lp_js_doPurchase" style="display:none;" data-laterpay=""></a>
+</div>
