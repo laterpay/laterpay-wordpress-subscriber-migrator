f387c7a0b092902e73e6d28b248463078b738f77
drodler <drodler@laterpay.net>
2015-03-23 13:20:03 +0100

    More wording changes

--- views/backend/migration.php
+++ views/backend/migration.php
@@ -295,12 +295,12 @@
                 <h3><?php _e( 'Email Notifications', 'laterpay_migrator' ); ?></h3>
                 <dfn>
                     <?php _e( 'The plugin will send every subscriber who has not yet switched to LaterPay up to two emails asking them to switch to a free time pass for the rest of their subscription period.', 'laterpay_migrator' ); ?><br>
-                    <?php _e( 'To ensure smooth delivery and rendering of the emails, we send these emails with <a href="http://mailchimp.com/" class="lp_inline" target="_blank">MailChimp</a>.', 'laterpay_migrator' ); ?><br>
-                    <?php _e( 'For every email type you have to create a campaign and a list on MailChimp.', 'laterpay_migrator' ); ?>
-                    <?php _e( 'The campaign defines the email layout to be used and the subscriber list the campaign is sent to.', 'laterpay_migrator' ); ?>
                     <?php if ( $laterpay['mailchimp_api_key'] == '' ): ?>
-                        <?php _e( 'If you don’t have a MailChimp account, you can subscribe to the free <a href="http://mailchimp.com/pricing/" class="lp_inline" target="_blank">MailChimp “Entrepreneur” plan</a>, which allows up to 2,000 recipients.', 'laterpay_migrator' ); ?>
+                        <?php _e( 'To ensure smooth delivery and rendering of the emails, we send these emails with <a href="http://mailchimp.com/" class="lp_inline" target="_blank">MailChimp</a>.', 'laterpay_migrator' ); ?><br>
+                        <?php _e( 'If you don’t have a MailChimp account, you can subscribe to the free <a href="http://mailchimp.com/pricing/" class="lp_inline" target="_blank">MailChimp “Entrepreneur” plan</a>, which allows up to 2,000 recipients.', 'laterpay_migrator' ); ?><br>
                     <?php endif; ?>
+                    <?php _e( 'For every email type you have to create a <strong>campaign</strong> and a <strong>list</strong> on MailChimp.', 'laterpay_migrator' ); ?>
+                    <?php _e( 'The campaign defines the email layout to be used as well as the subscriber list the campaign is sent to.', 'laterpay_migrator' ); ?><br>
                 </dfn>
                 <div class="lp_layout lp_mt-">
                     <div class="lp_layout__item lp_1/6">
