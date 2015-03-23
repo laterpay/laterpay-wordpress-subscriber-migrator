e1b1dbe024e91cf05d4694bf15fa773a50993b19
drodler <drodler@laterpay.net>
2015-03-23 15:52:13 +0100

    Fixed flash message rendering

--- views/backend/migration.php
+++ views/backend/migration.php
@@ -7,7 +7,7 @@
 
 <div class="lp_page wp-core-ui">
 
-    <div id="lp_js_flashMessage" class="lp_flash-Message" style="display:none;">
+    <div id="lp_js_flashMessage" class="lp_flash-message" style="display:none;">
         <p></p>
     </div>
 
